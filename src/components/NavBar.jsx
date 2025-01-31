f01de42892b4c13eb279d76797e387e1972caf7c
seunosinowo <oluwaseunpaul98@gmail.com>
2025-01-31 12:09:33 +0100

    Restructure NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,29 +12,31 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="sticky top-1 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80"> 
-      <div className="container px-4 mx-auto relative text-sm">
+    <nav className="sticky top-1 z-50 py-5 backdrop-blur-lg border-b border-neutral-700/80"> 
+      <div className="container px-4 mx-auto text-[0.98rem]">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
-              <img className="h-10 w-auto mr-2" src={logo} alt="logo" />
+              <img className="h-14 w-14 mr-2" src={logo} alt="logo" />
               <span className="text-xl -tracking-tight">NAMICC</span>
           </div>
 
-          <ul className="hidden lg:flex ml-14 space-x-14">
+          {/* Desktop View */}
+          <ul className="hidden lg:flex ml-14 gap-11">
               {navItems.map((item, index) => (
                 <li key = {index}>
-                  <a href={item.href}>{item.label}</a>
+                  <a href={item.href}
+                  className="text-green-800 hover:underline">{item.label}</a>
                 </li>
               ))}
           </ul>
 
-              <Link to = "/donation-details">
-              <button className="hidden lg:flex justify-center space-x-12 items-center">
-                <a href="#" className="bg-gradient-to-r from-green-500 to-green-800 py-2 px-3 rounded-md">
-                  Donate
-                </a>
+          <div className="hidden lg:flex justify-center items-center">
+            <Link to = "/donation-details">
+              <button className="bg-gradient-to-r from-green-500 to-green-800 py-2 px-3 rounded-md text-white">
+                Donate
               </button>
-              </Link>
+            </Link>
+          </div>
 
               <div className="lg:hidden md: flex flex-col justify-center">
                 <button onClick={toggleNarbar}>
@@ -42,23 +44,26 @@ const NavBar = () => {
                 </button>
               </div> 
         </div>
+        
         {mobileDrawerOpen && (
-          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+          <div className="fixed right-0 z-20 bg-white w-full p-6 flex flex-col justify-start items-start gap-4 lg:hidden">
             <ul>
               {navItems.map((item, index) => (
-                <li key={item.item} className="py-4">
-                  <a href={item.href}>{item.label}</a>
+                <li key={item.item} className="py-2 w-full">
+                  <a href={item.href}
+                  className="text-black text-[1rem]">
+                  {item.label}</a>
+                  
                 </li>
               ))}
             </ul>
-            <div className="flex space-x-6">
-              <a className="py-2 px-3 border rounded-md">
-                Sign In
-              </a>
 
-              <a className="py-2 px-3 rounded-md bg-gradient-to-r from-green-500 to-green-800">
-                Create an account
-              </a>
+            <div className="w-full">
+              <Link to = "/donation-details">
+                <button className="py-2 rounded-md bg-gradient-to-r from-green-500 to-green-800 w-full justify-center items-center text-white">
+                  Donate
+                </button>
+              </Link>
             </div>
 
           </div>
@@ -71,4 +76,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
