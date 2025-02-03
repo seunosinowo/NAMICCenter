2256f7055955e99f83db654ac8729c6a0cbdf94e
seunosinowo <oluwaseunpaul98@gmail.com>
2025-02-03 15:24:17 +0100

    Commiting Resolved mobile view

--- src/components/PrayerTimesCard.jsx
+++ src/components/PrayerTimesCard.jsx
@@ -17,7 +17,7 @@ const PrayerTimesCard = () => {
 
               
               {/* First img */}
-              <div className="relative z-10 flex flex-col justify-center items-center mt-8 mb-8 md:mb-16">
+              <div className="relative z-10 flex flex-col justify-center items-center mt-8 mb-8 md:mb-7">
               
                   <motion.div className='text-center text-white border-2 w-24 h-24 rounded-full border-white flex justify-center items-center mb-4 bg-yellow-400 mt-5'
 
@@ -28,12 +28,12 @@ const PrayerTimesCard = () => {
                   </motion.div>
 
                   <h1 className='text-2xl font-bold text-white mb-3 text-center'>Arabic Classes</h1>
-                  <h1 className='text-xl text-white text-center'>Learn the language of the Quran</h1>
+                  <h1 className='text-lg lg:text-xl text-white text-center'>Learn the language of the Quran</h1>
               </div>
 
               {/* Second img */}
-              <div className="relative z-10 flex flex-col justify-center items-center mt-8 mb-8 md:mb-16">
-                  <motion.div className='text-center text-white border-2 w-24 h-24 rounded-full border-white flex justify-center items-center mb-4 bg-yellow-400'
+              <div className="relative z-10 flex flex-col justify-center items-center mt-8 mb-8 md:mb-7">
+                  <motion.div className='text-center text-white border-2 w-24 h-24 rounded-full border-white flex justify-center items-center bg-yellow-400 mb-5'
 
                   animate={{ y: [0, -20, 0], rotate: [0, 15, -15, 0] }}
                   transition={{ duration: 3 }}
@@ -42,12 +42,12 @@ const PrayerTimesCard = () => {
                   </motion.div>
 
                   <h1 className='text-2xl font-bold text-white mb-3 text-center'>Lectures & Eventss</h1>
-                  <h1 className='text-xl text-white text-center'>Stay updated on our latest programs</h1>
+                  <h1 className='text-lg lg:text-xl text-white text-center'>Stay updated on our latest programs</h1>
               </div>
 
               {/* Third img */}
-              <div className="relative z-10 flex flex-col justify-center items-center mt-8 mb-8 md:mb-16">
-                  <motion.div className='text-center text-white border-2 w-24 h-24 rounded-full border-white flex justify-center items-center mb-4 bg-yellow-400'
+              <div className="relative z-10 flex flex-col justify-center items-center mt-8 mb-8 md:mb-7">
+                  <motion.div className='text-center text-white border-2 w-24 h-24 rounded-full border-white flex justify-center items-center mb-4 bg-yellow-400 mt-5'
                   animate={{ y: [0, -20, 0], rotate: [0, 15, -15, 0] }}
                   transition={{ duration: 3 }}
                   >
@@ -55,7 +55,7 @@ const PrayerTimesCard = () => {
                      <GraduationCap className='w-20 h-20 text-white p-3'/>
                   </motion.div>
                   <h1 className='text-2xl font-bold text-white mb-3 text-center'>Islamic Education</h1>
-                  <h1 className='text-xl text-white text-center'>Deepen your knowledge of Islam</h1>
+                  <h1 className='text-lg lg:text-xl text-white text-center'>Deepen your knowledge of Islam</h1>
               </div>
               
             </div>
