3a313b18457c285908ba66dfe12741493731186a
seunosinowo <oluwaseunpaul98@gmail.com>
2025-02-03 13:42:41 +0100

    Testing Responsiveness

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -9,33 +9,33 @@ const Overview = () => {
   return (
     <div className='mt-52 md:-mt-64 text-black mx-8 grid lg:grid-cols-2'>
 
-    <div className="col-span-1 flex flex-col items-center text-center md:text-left">
-      <p className="uppercase text-2xl mb-4">Building a Stronger Community</p>
-      <h1 className="bg-gradient-to-r text-green-800 text-5xl py-6 from-green-700 to-black text-transparent bg-clip-text mb-8">
-        Empowering Muslims, Enriching {" "}
-        <span>Lives</span>
-      </h1>
+      <div className='col-span-1'>
+        <p className='uppercase text-2xl'>Building a Stronger Community</p>
+        <h1 className='bg-gradient-to-r text-green-800 text-5xl py-6 from-green-700 to-black text-transparent bg-clip-text'>
+          Empowering Muslims, Enriching {" "}
+          <span>Lives</span>
+        </h1>
 
-      <p className="mt-1 mb-7 w-full text-sm md:text-base">
-        At NAMIC, we're dedicated to creating a vibrant community where Muslims 
-        can come together, learn, and grow. Our community development programs 
-        are designed to promote Islamic knowledge, cultural awareness, 
-        and social cohesion.
-      </p>
+        <p className='mt-1 mb-7 w-full justify-start'>
+            At NAMIC, we're dedicated to creating a vibrant community where Muslims 
+            can come together, learn, and grow. Our community development programs 
+            are designed to promote Islamic knowledge, cultural awareness, 
+            and social cohesion.
+        </p>
 
         {/* 2 Section inside */}
         <div className='grid grid-cols-2 mb-9'>
             <img src={praying} alt="Praying"
-            className='rounded-3xl h-[8rem] md:h-[12rem] w-[14rem] lg:w-[19rem]'
+            className='rounded-3xl h-[8rem] md:h-[12rem] w-[14rem] lg:w-[19rem] sm:hidden md:hidden'
             />
             <div>
-              <p className='w-[16rem] mb-9'>
+              <p className='w-[16rem] mb-9 sm:hidden md:hidden'>
                 From Arabic classes to monthly lectures, and from social 
                 events to spiritual gatherings, there's something for 
                 everyone at NAMIC.
               </p>
               
-              <div className='bg-yellow-400 w-16 h-16 rounded-full flex space-x-4'>
+              <div className='bg-yellow-400 w-16 h-16 rounded-full flex space-x-4 md:space-x-4'>
                 <img src={BIcon} alt="icon" 
                 className='w-16 h-16 text-center p-4'
                 />
